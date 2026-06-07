ba9946ddef9f556fb23c59ec36152b03ada9d2cd
Eric Mlynar <mlynare@gmail.com>
2026-06-07 15:38:54 -0400

    feat: update deployment workflow and SQL queries for historical weather trends

--- reporting/sources/bigquery_connection/historical_weather_trends.sql
+++ reporting/sources/bigquery_connection/historical_weather_trends.sql
@@ -1,5 +1,5 @@
 select trend_id, reading_date, location_latitude, location_longitude, avg_temperature, max_temperature, min_temperature, avg_precipitation_chance, mart_updated_at
-from project.dataset.table
-where reading_date < date_sub(current_date(), interval 3 month)
+from analytics.mart_weather_historical_trends
+where reading_date between date_sub(current_date(), interval 90 day) and current_date()
 order by reading_date asc
 limit 100;
